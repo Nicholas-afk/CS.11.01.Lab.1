055787d0dffc31cee4883f243aff3286cc371e0d
Nicholas-afk <64998158+Nicholas-afk@users.noreply.github.com>
2024-08-26 08:55:20 +0800

    Update Dateable.java

--- src/Dateable.java
+++ src/Dateable.java
@@ -4,14 +4,17 @@ public class Dateable {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter your age: ");
         int age = scanner.nextInt();
+
         if (age < 0) {
             System.out.println("Invalid age. Age cannot be negative.");
         } else {
             int minAgeToDate = 7 + (age / 2);
             System.out.println(age + "-year olds should date somebody who is at least " + minAgeToDate + " years old.");
         }
+
         scanner.close();
     }
 }
